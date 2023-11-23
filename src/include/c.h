306d206e18fa620cd4d25da42f0d19e7a927f563
Loup Lobet <louplobet@gmail.com>
2023-11-23 04:57:50 +0100

    remove headers from lib dir

--- src/include/c.h
+++ src/include/c.h
@@ -23,23 +23,4 @@ typedef unsigned int uint;
 typedef unsigned long ulong;
 typedef unsigned long long uvlong;
 
-/*
- * Include ANSI C(99) Standard Library
- */
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <float.h>
-#include <limits.h>
-#include <locale.h>
-#include <math.h>
-#include <setjmp.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
 #endif
--- src/include/libc.h
+++ src/include/libc.h
@@ -1,6 +1,28 @@
 #ifndef _LIBC_H_
 #define _LIBC_H_ 1
 
+/*
+ * ANSI C(99) Standard Library
+ */
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*
+ * OS libc
+ */
 #include <rune.h>
 #include <utf.h>
 
