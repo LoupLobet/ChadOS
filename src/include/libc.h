c6725a91b6b95f4b6548cdaace66d312cfc19eec
Loup Lobet <louplobet@gmail.com>
2023-11-23 20:02:01 +0100

    fixes

--- src/include/libc.h
+++ src/include/libc.h
@@ -11,33 +11,52 @@
 #endif
 
 /*
- * ANSI C(99) Standard Library
- */
-#include <assert.h>
-#include <complex.h>
-#include <ctype.h>
-#include <errno.h>
-#include <limits.h>
-#include <locale.h>
-#include <math.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-/*
- * POSIX Library
+ * OS specific
  */
 
-#include <dirent.h>
-
-/*
- * OS libc
- */
 #include <rune.h>
 #include <utf.h>
+
+/*
+ * malloc
+ */
+extern void	*chadcalloc(ulong, ulong);
+extern void	 chadfree(void *);
+extern void *chadmalloc(ulong);
+extern void	*chadrealloc(void *, ulong);
+
+#ifndef UNIX
+#define calloc  chadcalloc
+#define free    chadfree
+#define malloc  chadmalloc
+#define realloc chadrealloc
+#endif
+
+/*
+ * io
+ */
+
+/* oflag */
+#define OREAD     0    /* read */
+#define OWRITE    1    /* write */
+#define ORW       2    /* read and write */
+#define OEXEC     3    /* exec */
+#define ONONBLOCK 8    /* non-blocking */
+#define OAPPEND   16   /* open to the enf of file */
+#define OTRUNC    32   /* truncate file on open */
+#define OCEXEC    64   /* close file on exec */
+#define ORCLOSE   128  /* remove after closing */
+#define OLOCK     256  /* lock after opening, close if lock can't be set */
+#define OLOCKW    512  /* lock after opening, wait if lock can't be set */
+
+extern int	 chadclose(int);
+extern int	 chadopen(char *, int);
+extern long	 chadwrite(int, void *, long);
+
+#ifndef UNIX
+#define close chadclose
+#define open  chadopen
+#define write chadwrite
+#endif
 
 #endif
