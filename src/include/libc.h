ef73852a2e7c4eb21e089b146a6a800daa6b708c
Loup Lobet <louplobet@gmail.com>
2023-11-23 05:29:52 +0100

    add C99 and POSIX as mandatory for libc.h

--- src/include/libc.h
+++ src/include/libc.h
@@ -1,17 +1,25 @@
 #ifndef _LIBC_H_
 #define _LIBC_H_ 1
 
+#if __STDC_VERSION__ != 199901L
+#error "Host system must be provide C99"
+#endif
+
+#if defined(__unix__) || defined(__APPLE__) && defined(__MACH__)
+#else
+	#error "Host system must be POSIX"
+#endif
+
 /*
  * ANSI C(99) Standard Library
  */
 #include <assert.h>
+#include <complex.h>
 #include <ctype.h>
 #include <errno.h>
-#include <float.h>
 #include <limits.h>
 #include <locale.h>
 #include <math.h>
-#include <setjmp.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stddef.h>
@@ -19,6 +27,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+/*
+ * POSIX Library
+ */
+
+#include <dirent.h>
 
 /*
  * OS libc
